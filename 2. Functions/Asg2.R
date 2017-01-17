806fdebfe202a3fe5337938ebb09390d59522e34
TusharG <tushar.gawande90@gmail.com>
2017-01-17 05:47:23 +0000

    Implementing RMSE function

---
+++ 2. Functions/Asg2.R
@@ -0,0 +1,44 @@
+#Assignment 2
+
+#Function definition:
+my_rmse <- function(v1,v2,na.rm=F){
+  if(!is.numeric(v1))
+    stop("Invalid vector v1")
+  if(!is.numeric(v2))
+    stop("Invalid vector v2")
+  
+  if(na.rm==F){
+    if(any(is.na(v1)))
+      stop("Invalid vector v1 with NA")
+    if(any(is.na(v2)))
+      stop("Invalid vector v2 with NA")
+    if(sqrt(mean((v1-v2)^2)) == rmse(v1,v2))
+      stop("Correct RMSE")
+  }
+  else{
+    if(sqrt(mean((v1[!is.na(v1 & v2)] - v2[!is.na(v1 & v2)])^2)) == rmse(v1[!is.na(v1 & v2)],v2[!is.na(v1 & v2)]))
+      stop("Correct RMSE")
+  }
+}
+
+
+library(Metrics)
+#Test cases:
+#1 Non-numeric, Numeric, F
+my_rmse(c("ABC","ZY",1,2), c(1,2,3,4))#Actual
+
+
+#2 Numeric, Non-Numeric, F
+my_rmse(c(1,2,3,4), c("ABC",1,2,4))#Actual
+
+#3 Numeric(with NAs), Numeric(no	NAs), F
+my_rmse(c(1,2,3,NA), c(5,1,2,4))#Actual
+
+#4 Numeric(no	NAs), Numeric(with NAs), F
+my_rmse(c(1,2,3,4), c(NA,1,2,4))#Actual
+
+#5 Numeric(no	NAs), Numeric(no	NAs), F
+my_rmse(c(1,2,3,4), c(5,1,2,3))#Actual
+
+#6 Numeric(with	NAs), Numeric(with	NAs), T
+my_rmse(c(NA,2,3,4), c(5,NA,2,4),T)#Actual
