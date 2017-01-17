62eea872f237c79bdccb01329831c5c3b5238b15
TusharG <tushargawande90@users.noreply.github.com>
2017-01-17 05:07:08 +0000

    1. Vectors/

--- Asg1.R
+++ Asg1.R
@@ -1,11 +1,14 @@
 #Generating samples of sizes 10,10^2,...,10^6
 
-sample1 <- sample(2:12, size=10, replace = TRUE)
-sample2 <- sample(2:12, size=10^2, replace = TRUE)
-sample3 <- sample(2:12, size=10^3, replace = TRUE)
-sample4 <- sample(2:12, size=10^4, replace = TRUE)
-sample5 <- sample(2:12, size=10^5, replace = TRUE)
-sample6 <- sample(2:12, size=10^6, replace = TRUE)
+sampleGenerator <- function(size,diceRoll=1:6,replace=T){
+  sample(diceRoll,size = size,replace = replace)+sample(diceRoll,size = size,replace = replace)
+}
+sample1 <- sampleGenerator(10^1)
+sample2 <- sampleGenerator(10^2)
+sample3 <- sampleGenerator(10^3)
+sample4 <- sampleGenerator(10^4)
+sample5 <- sampleGenerator(10^5)
+sample6 <- sampleGenerator(10^6)
 
 #Visually verifying whether the sample distribution looks as expected
 
@@ -21,8 +24,12 @@ qplot(sample6,binwidth=1)
 
 freqSample1 <- 0;freqSample2 <- 0;freqSample3 <- 0;freqSample4 <- 0;freqSample5 <- 0;freqSample6 <- 0;
 for(i in 1:11) {
-  freqSample1[i] <- sum(sample1==(i+1));freqSample2[i] <- sum(sample2==(i+1));freqSample3[i] <- sum(sample3==(i+1));
-  freqSample4[i] <- sum(sample4==(i+1));freqSample5[i] <- sum(sample5==(i+1));freqSample6[i] <- sum(sample6==(i+1));
+  freqSample1[i] <- sum(sample1==(i+1));
+  freqSample2[i] <- sum(sample2==(i+1));
+  freqSample3[i] <- sum(sample3==(i+1));
+  freqSample4[i] <- sum(sample4==(i+1));
+  freqSample5[i] <- sum(sample5==(i+1));
+  freqSample6[i] <- sum(sample6==(i+1));
 }
 
 expProbabilisticVector <- c(1,2,3,4,5,6,5,4,3,2,1)/36
@@ -38,4 +45,4 @@ rmseSample5 <- sqrt(mean((freqSample5/100000-expProbabilisticVector)^2))
 rmseSample6 <- sqrt(mean((freqSample6/1000000-expProbabilisticVector)^2))
 
 #Plotting rmses
-qplot(c(rmseSample1,rmseSample2,rmseSample3,rmseSample4,rmseSample5,rmseSample6),c(10,10^2,10^3,10^4,10^5,10^6))
+qplot(c(rmseSample6,rmseSample5,rmseSample4,rmseSample3,rmseSample2,rmseSample1),c(10^6,10^5,10^4,10^3,10^2,10))
